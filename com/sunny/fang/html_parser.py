c453cbcf403c98959b31cb87a119ff6e59aba9cd
walle <zlkjzxj@163.com>
2019-04-26 00:34:40 +0800

    xxx

--- com/sunny/fang/html_parser.py
+++ com/sunny/fang/html_parser.py
@@ -24,4 +24,10 @@ class HtmlParser(object):
         return new_urls
 
     def get_new_data(self, page_url, soup):
-        res_data = {}
+        res_data = {'url': page_url}
+
+        title_node = soup.find("dd", class_="lemmaWgt-lemmaTitle-title").find("h1")
+        res_data['title'] = title_node.get_text()
+        summary_node = soup.find("div", "lemma-summary")
+        res_data['summary'] = summary_node.get_text()
+        return res_data
